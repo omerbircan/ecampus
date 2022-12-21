1a3f6471be7ff5ddfb676351df8483fe9f440834
omerbircan <omer.bircan@hotmail.com.tr>
2022-12-21 22:40:10 +0300

    change1

--- app/src/main/java/com/omerbircan/ecampus/MainActivity.kt
+++ app/src/main/java/com/omerbircan/ecampus/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.omerbircan.ecampus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.omerbircan.ecampus.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+
+    fun signupclick(view : View){
+
+        val intent = Intent(applicationContext, SigninMail::class.java)
+        startActivity(intent)
+    }
+
+    fun loginclick(view : View){
+
+        val intent = Intent(applicationContext, HomePage::class.java)
+        startActivity(intent)
+
+
     }
 }
---
+++ app/src/main/java/com/omerbircan/ecampus/SigninMail.kt
@@ -0,0 +1,11 @@
+package com.omerbircan.ecampus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigninMail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin_mail)
+    }
+}
